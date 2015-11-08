2bcf4f139fb0b9867bc3d24b0f2989cea43e742a
Marcus Götling <marcus@gotling.se>
2015-11-08 23:09:00 +0800

    Save reports in database

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Flask, g, render_template
+from flask import Flask, g, render_template, request, url_for, redirect, session
 from flask.ext.babel import Babel, format_datetime
 import db_helper
 
@@ -22,8 +22,12 @@ def close_connection(exception):
 def index():
     context = {
         'timestamp': datetime.now(),
-        'customers': db_helper.query('SELECT * FROM customers ORDER BY name')
+        'customers': db_helper.query('SELECT * FROM customers ORDER BY name'),
+        'message': session.get('message'),
+        'error': session.get('error')
     }
+
+    print(context)
 
     return render_template('index.html', **context)
 
@@ -32,6 +36,21 @@ def index():
 def get_customers():
     customers = db_helper.query('select * from customers')
     return str(customers)
+
+
+@app.route("/report", methods=['POST'])
+def report():
+    customer_id = request.form['customer-id']
+    units = request.form['units']
+
+    if customer_id and units:
+        db_helper.query('INSERT INTO reports (customer_id, units, datetime) VALUES (?, ?, CURRENT_TIMESTAMP)', [customer_id, units])
+        customer_name = db_helper.query('SELECT name FROM customers WHERE id = ?', [customer_id])[0].get('name')
+        session['message'] = "Added %s units to %s" % (units, customer_name)
+        return redirect(url_for('index'))
+    else:
+        session['error'] = "Both customer and units must be chosen"
+        return redirect(url_for('index'))
 
 
 @app.route('/<path:path>')
--- schema.sql
+++ schema.sql
@@ -9,3 +9,10 @@ INSERT INTO customers (name, email)
 VALUES ('Ana Warung', 'anawarung@gotling.se');
 INSERT INTO customers (name, email)
 VALUES ('Adeng-Adeng Bungalows', 'adeng-adeng@gotling.se');
+
+CREATE TABLE reports
+( id INTEGER PRIMARY KEY NOT NULL
+, customer_id INTEGER
+, units INTEGER
+, datetime INTEGER
+, FOREIGN KEY(customer_id) REFERENCES customers(id));
