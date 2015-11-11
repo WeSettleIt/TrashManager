b05f397d2fd688c6fba56002db391d4e2345d3c0
Marcus Götling <marcus@gotling.se>
2015-11-11 21:53:04 +0800

    Remove button when displaying reports. Improve toasts

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,17 @@
-(function($){
-  $(function(){
+(function ($) {
+    $(function () {
 
-    $('.button-collapse').sideNav();
-    $('select').material_select();
+        $('.button-collapse').sideNav();
+        $('select').material_select();
 
-  }); // end of document ready
+        $('#list #customer-id').change(function () {
+            this.form.submit();
+        });
+
+        toastr.options.closeButton = false;
+        toastr.options.timeOut = 30000;
+        toastr.options.extendedTimeOut = 600000;
+        toastr.options.progressBar = true;
+        toastr.options.positionClass = "toast-bottom-full-width";
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
